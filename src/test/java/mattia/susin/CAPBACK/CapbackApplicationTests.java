be9b11579ce529b2e8cf4ebc9255e2974819ac88
MattiaSusin <m.susin28@gmail.com>
2024-09-30 09:47:01 +0200

    Initial commit

---
+++ src/test/java/mattia/susin/CAPBACK/CapbackApplicationTests.java
@@ -0,0 +1,13 @@
+package mattia.susin.CAPBACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
